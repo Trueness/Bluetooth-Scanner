cb1eecdb0f82d67772c64f7bf0bf8f3ed25ef73c
Trueness <trueness@comcast.net>
2017-04-03 21:46:32 -0400

    I. 04.03.2017
    Intermediate Check In
    1. Added Broadcast Receiver logic

--- app/src/main/java/sampleapp/prempoint/bluetoothscanner/bluetooth/BLEService.java
+++ app/src/main/java/sampleapp/prempoint/bluetoothscanner/bluetooth/BLEService.java
@@ -3,16 +3,16 @@ package sampleapp.prempoint.bluetoothscanner.bluetooth;
 import android.app.Activity;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
-
-import sampleapp.prempoint.bluetoothscanner.activity.MainActivity;
 
 /**
  * Description:
@@ -20,7 +20,9 @@ import sampleapp.prempoint.bluetoothscanner.activity.MainActivity;
  * @author
  */
 
-//TODO: 1. Use a broadcast receiver to returm the results (Ref PP 438 APC)
+//TODO: 1. Use a broadcast receiver to returm the results (Ref PP 237 APC)
+//TODO:    1.a Also Ref: https://developer.android.com/guide/topics/connectivity/bluetooth-le.html
+//TODO:      https://android.googlesource.com/platform/development/+/512ea9b6f8cc75ec74a7ab8d1c38dec201667f1e/samples/BluetoothHDP/src/com/example/bluetooth/health?autodive=0/
 //TODO: 2. Expose the Following Parameters (Maybe as a Model Class):
 //TODO     a. Device’s bluetooth name
 //TODO     b. RSSI signal strength
@@ -28,7 +30,7 @@ import sampleapp.prempoint.bluetoothscanner.activity.MainActivity;
 
 public class BLEService extends Service {
 
-    private  Context mCtx;
+    private Context mCtx;
     private BluetoothAdapter bluetoothAdapter;
     public BluetoothManager bluetoothManager;
     private final static int REQUEST_ENABLE_BT = 1;
@@ -47,9 +49,6 @@ public class BLEService extends Service {
 
     }
     */
-
-
-
     @Override
     public void onCreate() {
 
@@ -63,37 +62,56 @@ public class BLEService extends Service {
 
         initializeBLeAdapter();
         determineBLeEnablement();
+        StartBLEDiscovery();
 
     }
-
-
 
     /*
      * This method initializes the BLE Adapter
      */
-    public void initializeBLeAdapter(){
+    public void initializeBLeAdapter() {
         // Initializes Bluetooth adapter.
         bluetoothManager = (BluetoothManager) mCtx.getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
     }
 
     public void determineBLeEnablement() {
-
-
-
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             //  mActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             Toast.makeText(getApplicationContext(), "Please Enable Bluetooth", Toast.LENGTH_LONG).show();
-
         }
-
     }
+ public void StartBLEDiscovery() {
+     final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+         @Override
+         public void onReceive(Context context, Intent intent) {
+             String action = intent.getAction();
 
-    //Intent receivers below
+             // When discovery finds a device
+             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                 // Get the BluetoothDevice object from the intent
+                // BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
+                // if(intent.hasExtra("scanBle")) {
+                     BluetoothDevice device = intent.getParcelableExtra("scanBle");
+               //  }
+                 Log.v("BlueTooth Testing", device.getName() + "\n"
+                         + device.getAddress());
+             }
+         }
+     };
+     IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+     getApplication().registerReceiver(mReceiver, filter);
+ }
+
+
+
+   // bluetoothAdapter.startDiscovery();
+
+
+        //Intent receivers below
     protected void onHandleIntent(Intent intent) {
-
 
         boolean scanWifi = intent.getBooleanExtra("scanBle", true);
     }
